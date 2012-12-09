6f8e54e25ded0bcf7e9da19c8d6536307ebb5228
Jaime de Greiff <alegreiff@gmail.com>
2012-12-09 15:10:34 -0500

    nuevo

--- micodigo.js
+++ micodigo.js
@@ -9,8 +9,8 @@ $(document).on("ready", inicio);
 function cambiallanta(datos){
 	var ll1 = datos.currentTarget.id;
 	alert(ll1);
-	$("#r1").html("<img src ='llanta1.png' width='120'/>");
-	//$("#r2").html("<img src ='llanta1.png' width='120'/>");
+	$("#r1").html("<img src ='llanta1.png' width='110'/>");
+	$("#r2").html("<img src ='llanta1.png' width='110'/>");
 	//$("#r1").html("Car 1");
 }
 function transicion()
