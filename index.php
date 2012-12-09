237d9b44adf9e9be0a032a389b17ca6680888cea
Jaime de Greiff <alegreiff@gmail.com>
2012-12-09 11:29:22 -0500

    curso mejorando.la

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Jaime</title>
+<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> <!-- forza al sitio abierto en ie arrancar en chrome -->
+	<title>Nuevo AVE0 2013</title>
+	<meta name="description" content="Menos gasolina, más HTML5"> <!-- descripción en google -->
+	
+	<link rel="stylesheet" href="estilos.css">
+	<script type="text/javascript" src="prefixfree.min.js"></script>
 </head>
 <body>
-	<h1>Hola Jaime</h1>
+<header>
+	<hgroup>
+	<h1>Nuevo AVEO 2013</h1>
+	<h2>Menos gasolina, más HTML5</h2>
+	</hgroup>
+	
+</header>
+<div class="jaime">Hola Mundo</div>
+<script src="http://ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.0.6-development-only.js"></script>
+<script>
+if (Modernizr.localstorage) {
+  console.log("Sisas");
+  // Aqui puedes guardar algo al navegador
+} else {
+  // No hay localstorage, habrá que usar alguna cookie o similar
+  console.log("Parras");
+}
+
+</script>
 </body>
 </html>
---
+++ micodigo.js
@@ -0,0 +1,37 @@
+$(document).on("ready", inicio);
+
+	function inicio()
+	{
+		
+		$("#personalicite").on("click", transicion);
+	}
+
+function transicion()
+{
+	//Objeto JAVASCRIPT JSON
+	var cambiosHistoria = {
+		
+		margin: 0,
+		overflow: "hidden",
+		padding: 0,
+		width: 0
+	}
+
+	var cambiosPerson = {
+		
+		height: "auto",
+		opacity: 1,
+		
+		width: "40%"
+	}	
+	$("#historia").css(cambiosHistoria);
+	$("#personalizacion").css(cambiosPerson);
+	$("#color div").on("click", cambiarColor)
+}
+
+function cambiarColor(datos){
+	var col = datos.currentTarget.id;
+	$("#cochecito img").attr("src", "c" + col + ".jpg")
+	$("#textocolor").html("<strong>Color</strong> " + col);
+	
+}
