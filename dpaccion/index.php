ab2da8c467223a9855deb91de1f799f8fdfaeddb
Jaime de Greiff <alegreiff@gmail.com>
2012-12-09 20:53:18 -0500

    Acciones de grupo

---
+++ dpaccion/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Acción de Grupo – Caso Doña Juana</title>
+</head>
+<body>
+<header>
+	<h1>Acción de Grupo – Caso Doña Juana</h1>
+</header>
+<section>
+	
+</section>	
+</body>
+</html>
